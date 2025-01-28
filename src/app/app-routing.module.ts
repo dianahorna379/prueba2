094623cf142e0d7edeb1680218245cf7e3d4fdd2
dianahorna379 <diana.horna.379@gmail.com>
2025-01-28 18:23:37 -0500

    primer cambio prueba 2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Prueba2Component } from './pages/prueba2/prueba2.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+  path: 'inicio',
+  component:Prueba2Component
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
